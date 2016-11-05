0d8b056643bbd535f22412ba75a62f4d0a90cb71
Eliad Moosavi <iliadm@ca.ibm.com>
2016-11-05 11:28:48 -0500

    Added OCR

--- components/dangerZone.ctr.js
+++ components/dangerZone.ctr.js
@@ -5,8 +5,57 @@ angular.module('dangerZone')
             .accentPalette('indigo');
         })
 
-        .controller("dangerZoneCtrl", function($rootScope, $scope, $mdSidenav, $mdToast, $state) {
+        .controller("dangerZoneCtrl", function($rootScope, $scope, $mdSidenav, $mdToast, $state, $http) {
             $scope.analyze = function(url) {
-                alert(url);
+                $scope.getBase64FromImageUrl(url);
+            }
+            
+            $scope.getBase64FromImageUrl = function(url) {
+                var img = new Image();
+
+                img.setAttribute('crossOrigin', 'anonymous');
+
+                img.onload = function () {
+                    $scope.imageLoaded = true;
+                    
+                    var canvas = document.createElement("canvas");
+                    canvas.width =this.width;
+                    canvas.height =this.height;
+
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(this, 0, 0);
+
+                    var dataURL = canvas.toDataURL("image/jpg");
+                    dataURL = dataURL.replace("data:image/png;base64,", "");
+
+//                    console.log(dataURL);
+                    
+                    var parameter = JSON.stringify({
+                                 "requests": [
+                                  {
+                                   "features": [
+                                    {
+                                     "type": "TEXT_DETECTION"
+                                    }
+                                   ],
+                                   "image": {
+                                     "content": dataURL
+                                   }
+                                  }
+                                 ]
+                                });
+                    $http.post('https://vision.googleapis.com/v1/images:annotate?key= AIzaSyBMSx7JI7HlEnGLKUJ0goYuJhbJq1kWHYg', parameter).
+                    success(function(data, status, headers, config) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        console.log(data.responses[0].textAnnotations[0].description);
+                      }).
+                      error(function(data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                      });
+                };
+
+                img.src = url;
             }
         });
