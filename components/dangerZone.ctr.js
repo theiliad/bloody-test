8d23fa96af43642bbc7e6e94d583e8513bf0b510
Eliad Moosavi <iliadm@ca.ibm.com>
2016-11-05 11:57:55 -0500

    Get OCR info and parse it to get all the variables

--- components/dangerZone.ctr.js
+++ components/dangerZone.ctr.js
@@ -1,3 +1,6 @@
+var variables = [];
+var variableNames = ["Sodium", "Potassium", "Chloride", "Carbon Dioxide", "Calcium", "Protein", "Albumin", "Globulin", "A/G Ratio", "Bilirubin", "Alkaline", "AST", "ALT", "Creatine"];
+
 angular.module('dangerZone')
         .config(function($mdThemingProvider) {
           $mdThemingProvider.theme('default')
@@ -48,14 +51,38 @@ angular.module('dangerZone')
                     success(function(data, status, headers, config) {
                         // this callback will be called asynchronously
                         // when the response is available
-                        console.log(data.responses[0].textAnnotations[0].description);
-                      }).
-                      error(function(data, status, headers, config) {
+                        console.log(data.responses[0].textAnnotations[0].description.split('\n'));
+                        
+                        $scope.getVariables(data.responses[0].textAnnotations[0].description.split('\n'));
+                      })
+                        .error(function(data, status, headers, config) {
                         // called asynchronously if an error occurs
                         // or server returns response with an error status.
                       });
                 };
 
                 img.src = url;
+            }
+            
+            $scope.getVariables = function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    for (var k = 0; k < variableNames.length; k++) {
+                        if (data[i].toLowerCase().indexOf(variableNames[k].toLowerCase()) != -1) {
+                            for (var j = i; j < data.length; j++) {
+                                if (!isNaN(data[j])) {
+                                    variables[variableNames[k]] = data[j];
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                console.log(variables);
+                
+                for (var i = 0; i < variableNames.length; i++) {
+                    console.log(data.inde);
+                }
             }
         });
