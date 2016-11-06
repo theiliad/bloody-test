a6ee7f9cb2e41627ec1024f8ebcad3bb6592a302
Eliad Moosavi <iliadm@ca.ibm.com>
2016-11-05 20:11:22 -0500

    Upload photo from camera or gallery

--- components/dangerZone.ctr.js
+++ components/dangerZone.ctr.js
@@ -212,11 +212,9 @@ angular.module('dangerZone')
                 evt.preventDefault();
                 $state.go(to.redirectTo, params, {location: 'replace'})
               }
-                
-              if (to.url == '/results') {
-                  console.log(params);
-                  
-                  alert("wadup");
+                            
+              if (to.url == '/results' && params.statusesObject == null) {
+                  $state.go('index', {});
               }
             });
         }])
@@ -228,64 +226,82 @@ angular.module('dangerZone')
         .controller("indexCtrl", function($rootScope, $scope, $mdSidenav, $mdToast, $state, $http) {
             $scope.PROGRESS_STEPS = PROGRESS_STEPS;
             
-            $scope.analyze = function(url) {
-                $scope.getBase64FromImageUrl(url);
-                
-                $scope.progress = true;
-                $scope.progressIndex = 0;
+            $scope.changed = function(evt) {
+                $scope.files = evt.target.files;
             }
             
-            $scope.getBase64FromImageUrl = function(url) {
-                var img = new Image();
+            $scope.analyze = function() {                
+                $scope.progress = true;
+                $scope.progressIndex = 0;
+                
+                console.log($scope.files);
+                
+                var files = $scope.files;
+                var file = files[0];
 
-                img.setAttribute('crossOrigin', 'anonymous');
+                if (files && file) {
+                    var reader = new FileReader();
 
-                img.onload = function () {
-                    $scope.progressIndex = 1;
+                    reader.onload = function(readerEvt) {
+                        $scope.progressIndex = 1;
+                        var binaryString = readerEvt.target.result;
+//                            document.getElementById("base64textarea").value = btoa(binaryString);
+
+                        var parameter = JSON.stringify({
+                             "requests": [
+                              {
+                               "features": [
+                                {
+                                 "type": "TEXT_DETECTION"
+                                }
+                               ],
+                               "image": {
+                                 "content": btoa(binaryString)
+                               }
+                              }
+                             ]
+                            });
+
+                        $http.post('https://vision.googleapis.com/v1/images:annotate?key= AIzaSyBMSx7JI7HlEnGLKUJ0goYuJhbJq1kWHYg', parameter).
+                        success(function(data, status, headers, config) {
+                            $scope.progressIndex = 2;
+
+                            // this callback will be called asynchronously
+                            // when the response is available
+                            console.log(data.responses[0].textAnnotations[0].description.split('\n'));
+
+                            $scope.getVariables(data.responses[0].textAnnotations[0].description.split('\n'));
+                          })
+                            .error(function(data, status, headers, config) {
+                            // called asynchronously if an error occurs
+                            // or server returns response with an error status.
+                          });
+                    };
+
+                    reader.readAsBinaryString(file);
+                }
+            }
+            
+            $scope.getBase64FromImageUrl = function(evt) {
                     
-                    var canvas = document.createElement("canvas");
-                    canvas.width =this.width;
-                    canvas.height =this.height;
-
-                    var ctx = canvas.getContext("2d");
-                    ctx.drawImage(this, 0, 0);
-
-                    var dataURL = canvas.toDataURL("image/jpg");
-                    dataURL = dataURL.replace("data:image/png;base64,", "");
+//                var img = new Image();
+//
+//                img.setAttribute('crossOrigin', 'anonymous');
+//
+//                img.onload = function () {
+//                    $scope.progressIndex = 1;
+//                    
+//                    var canvas = document.createElement("canvas");
+//                    canvas.width =this.width;
+//                    canvas.height =this.height;
+//
+//                    var ctx = canvas.getContext("2d");
+//                    ctx.drawImage(this, 0, 0);
+//
+//                    var dataURL = canvas.toDataURL("image/jpg");
+//                    dataURL = dataURL.replace("data:image/png;base64,", "");
 
 //                    console.log(dataURL);
-                    
-                    var parameter = JSON.stringify({
-                                 "requests": [
-                                  {
-                                   "features": [
-                                    {
-                                     "type": "TEXT_DETECTION"
-                                    }
-                                   ],
-                                   "image": {
-                                     "content": dataURL
-                                   }
-                                  }
-                                 ]
-                                });
-                    $http.post('https://vision.googleapis.com/v1/images:annotate?key= AIzaSyBMSx7JI7HlEnGLKUJ0goYuJhbJq1kWHYg', parameter).
-                    success(function(data, status, headers, config) {
-                        $scope.progressIndex = 2;
-                        
-                        // this callback will be called asynchronously
-                        // when the response is available
-                        console.log(data.responses[0].textAnnotations[0].description.split('\n'));
-                        
-                        $scope.getVariables(data.responses[0].textAnnotations[0].description.split('\n'));
-                      })
-                        .error(function(data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                      });
-                };
-
-                img.src = url;
             }
             
             $scope.getVariables = function(data) {
